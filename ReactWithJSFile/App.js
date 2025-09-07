3b878c45980a3c0648ea2ae42693753c885f65bc
Sowjanya <g.sowjanya@sap.com>
2025-09-07 15:37:51 +0530

    Ingnite app using parcel bundler

--- ReactWithJSFile/App.js
+++ ReactWithJSFile/App.js
@@ -1,4 +1,11 @@
-const parent=React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[React.createElement("h1",{},"Hello I'm H1 Tag"),React.createElement("h2",{},"Hello I'm H2 Tag")]))
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent=React.createElement("div",{id:"parent"},
+    React.createElement("div",{id:"child"},
+        [React.createElement("h1",{},"Hello I'm H1 Tag"),
+            React.createElement("h2",{},"Hello I'm H2 Tag")]))
+
 console.log(parent); //It will return an object
 
 const heading=React.createElement("h1",{id:"heading",xyz:"abc"}, "Hello World from React");
